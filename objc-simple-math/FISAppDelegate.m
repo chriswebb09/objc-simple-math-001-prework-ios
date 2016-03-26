87529fd47628ec1562956a06fd69e188b1fa3823
chriswebb09 <chris.webb5249@gmail.com>
2016-03-26 09:09:34 -0400

    Done.

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -7,11 +7,20 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here.
-     
-     */
+    NSInteger i = 0;
+    NSInteger b = i + 1;
+    NSInteger calc;
+    NSLog(@"i: %li", i);
+    NSLog(@"b: %li", b);
+    BOOL isTrue = YES;
+    NSLog(@"isTrue: %d", isTrue);
+    CGFloat f = 0.0;
+    f = 17/29.0;
+    NSLog(@"f: %f", f);
+    NSInteger num1 = 20;
+    NSInteger num2 = 4;
+    calc = num1 / num2;
+    NSLog(@"calcalate %li", calc);
     
     // do not alter
     return YES;  //
